a3d7d3c685bc64fb1adb983b5a83a4f9066f0a98
mahamudulhassain2013 <mahamudulhasan4514@gmail.com>
2022-11-21 14:50:36 +0600

    21-11-2022

---
+++ Array/arrayelement.ts
@@ -0,0 +1,14 @@
+
+
+
+function AllElement(arr:Array<string>){
+    let i;
+    for( i= 0; i < arr.length; i++){
+        console.log(arr[i]);
+      
+    }
+    
+}
+
+ console.log(AllElement(["C", "C++", "C#", "Java", "Javascript", "Php", "Dart", "Phython", "Cotlin", "Swift"]));
+ 
