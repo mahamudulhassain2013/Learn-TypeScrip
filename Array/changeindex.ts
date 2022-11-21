7c11731d48c7ae0f0ab887586ef6cad65b719516
mahamudulhassain2013 <mahamudulhasan4514@gmail.com>
2022-11-21 15:52:27 +0600

    Change array element

---
+++ Array/changeindex.ts
@@ -0,0 +1,16 @@
+
+function ArrayChangeIndex(){
+   let arr:Array<string> = ["Mahamudul", "Rajib", "Mohon", "Nadim", "Shepon"];
+
+    let remove = arr.splice(0, 1);
+    let insert = arr.splice(3, 0, "Mahamudul");
+
+        console.log(arr);
+
+
+    }
+
+    ArrayChangeIndex();
+    
+
+    
