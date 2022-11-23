3031f234e200fab5bb80ed51e86b9729d37e49b3
mahamudulhassain2013 <mahamudulhasan4514@gmail.com>
2022-11-23 15:00:48 +0600

    create a simple function

---
+++ Function/average.ts
@@ -0,0 +1,13 @@
+
+function average (num1:number, num2:number, num3:number) {
+    let sum:number, avg:number;
+
+    avg = (num1 + num2 + num3) / 3;
+
+    
+    return avg;
+
+}
+    console.log(average(20,30,50));
+
+
