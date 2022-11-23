fa64b49514aa41e6b263b4ec756dc12a47d5d398
mahamudulhassain2013 <mahamudulhasan4514@gmail.com>
2022-11-23 16:58:53 +0600

    using the comment in function

---
+++ Function/anonymous.ts
@@ -0,0 +1,25 @@
+
+
+/** 
+ let football = function() {
+
+    console.log("Hello Fifa World Cup 2022");
+}
+
+football();
+
+*/
+
+ /** 
+ (function () {
+    console.log("Hello Brazil Wellcome to Fifa World Cup 2022");
+}) ()
+  */
+
+
+
+let football = function(countryname:string) {
+
+    console.log("Hello" + countryname + "Wellcome to Fifa World Cup 2022");
+}
+football(" Brazil ");
--- Function/average.ts
+++ Function/average.ts
@@ -1,13 +1,24 @@
 
+/**
+ * @desc Function that input three numbers and calculate the average number.
+ * @param num1 Number       this is the first number
+ * @param num2 Number       thsi is the second number
+ * @param num3 Number       this is the third number
+ * @returns average Number          returns  the average value.
+ */
+
+
+
 function average (num1:number, num2:number, num3:number) {
-    let sum:number, avg:number;
+    let avg:number;
 
     avg = (num1 + num2 + num3) / 3;
 
-    
-    return avg;
 
+    return avg;
+   
 }
-    console.log(average(20,30,50));
+
+     console.log(average(20,30,50));
 
 
