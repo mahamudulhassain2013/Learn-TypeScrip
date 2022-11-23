a2db23a69fd3767c7f7fa98fcb062242d5f34764
mahamudulhassain2013 <mahamudulhasan4514@gmail.com>
2022-11-23 17:02:05 +0600

    update comment

--- Function/anonymous.ts
+++ Function/anonymous.ts
@@ -1,25 +1,15 @@
 
 
-/** 
- let football = function() {
-
-    console.log("Hello Fifa World Cup 2022");
-}
-
-football();
-
-*/
-
- /** 
- (function () {
-    console.log("Hello Brazil Wellcome to Fifa World Cup 2022");
-}) ()
-  */
-
+/**
+ * @desc Using Anonymous Function that wellcome  World cup football teams.
+ * @param       {string} countryname        Name of the country.
+ * @returns     {string}                    Wellcome message to the user.
+ */
 
 
 let football = function(countryname:string) {
 
     console.log("Hello" + countryname + "Wellcome to Fifa World Cup 2022");
 }
+
 football(" Brazil ");
