ed18e04069659796dbd5be9e3041c2d011203902
mahamudulhassain2013 <mahamudulhasan4514@gmail.com>
2022-11-24 18:13:34 +0600

    factorial number using while loop

---
+++ Loop/factorial.ts
@@ -0,0 +1,32 @@
+
+//     let i:any, num = 5, fact = 1; 
+//    for(i = 1; i <= num; i++){
+//     fact = fact * i;
+   
+
+//    }
+
+//    console.log(fact);
+
+        
+
+   function factorial (num: Number)
+   {
+    let i:number = 1, fact:number = 1; 
+    while(i <= num)
+    {
+        fact = fact * i;
+        i++;
+
+    }
+
+        console.log(fact);
+
+   }
+
+   factorial(5);
+
+   
+  
+
+  
